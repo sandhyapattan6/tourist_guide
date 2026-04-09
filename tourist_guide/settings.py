d26f4ad7343e701d88f7c90431d5e0c600c1f35c
sandhyapattan <pattansandhya@gmail.com>
2026-04-09 22:42:58 +0530

    fix booking migrations

--- tourist_guide/settings.py
+++ tourist_guide/settings.py
@@ -162,3 +162,22 @@ LOGGING = {
         'level': 'ERROR',
     },
 }
+
+
+import os
+import dj_database_url
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
