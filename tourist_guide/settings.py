0071392ae19e62d1ae0e077685905d92b5b0143c
sandhyapattan <pattansandhya@gmail.com>
2026-04-13 20:21:58 +0530

    final fixing

--- tourist_guide/settings.py
+++ tourist_guide/settings.py
@@ -27,12 +27,7 @@ SECRET_KEY = 'django-insecure-3tq@8og2w6^ll1puw$$$56fz49x$m#!%^))g%ku#)avo&vs1vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "tourist-guide-cgsj.onrender.com",
-    ".onrender.com",
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
