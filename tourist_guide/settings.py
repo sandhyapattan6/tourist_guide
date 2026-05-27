a95d47b01e0fe6628b3fcb4894d0f3913671bc1e
sandhyapattan <pattansandhya@gmail.com>
2026-05-27 18:36:06 +0530

    Initial commit

--- tourist_guide/settings.py
+++ tourist_guide/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3tq@8og2w6^ll1puw$$$56fz49x$m#!%^))g%ku#)avo&vs1vo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "tourist-guide-cgsj.onrender.com",
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
